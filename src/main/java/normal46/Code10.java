bb28a9f9df6133237d892df0227c142976a16590
Rock-Ayl <AnYongLiang4869@163.com>
2025-09-12 11:11:43 +0800

    修改：2025-9-12

--- src/main/java/normal46/Code10.java
+++ src/main/java/normal46/Code10.java
@@ -48,24 +48,24 @@ import java.util.Map;
 public class Code10 {
 
     //所有2的幂
-    private static long[] ARR = new long[]{1L, 2L, 4L, 8L, 16L, 32L, 64L, 128L, 256L, 512L, 1024L, 2048L, 4096L, 8192L, 16384L, 32768L, 65536L, 131072L, 262144L, 524288L, 1048576L, 2097152L, 4194304L, 8388608L, 16777216L, 33554432L, 67108864L, 134217728L, 268435456L, 536870912L, 1073741824L, 2147483648L, 4294967296L, 8589934592L, 17179869184L, 34359738368L, 68719476736L, 137438953472L, 274877906944L, 549755813888L, 1099511627776L, 2199023255552L, 4398046511104L, 8796093022208L, 17592186044416L, 35184372088832L, 70368744177664L, 140737488355328L, 281474976710656L, 562949953421312L, 1125899906842624L, 2251799813685248L, 4503599627370496L, 9007199254740992L, 18014398509481984L, 36028797018963968L, 72057594037927936L, 144115188075855872L, 288230376151711744L, 576460752303423488L, 1152921504606846976L, 2305843009213693952L, 4611686018427387904L};
+    private static int[] ARR = new int[]{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824};
 
     public int countPairs(int[] deliciousness) {
         //数量缓存
-        Map<Long, Integer> countMap = new HashMap<>();
+        Map<Integer, Integer> countMap = new HashMap<>();
         //循环
-        for (long num : deliciousness) {
+        for (int num : deliciousness) {
             //+1
             countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
         //结果
         int result = 0;
         //循环
-        for (long num : deliciousness) {
+        for (int num : deliciousness) {
             //循环所有可能
-            for (long target : ARR) {
+            for (int target : ARR) {
                 //另一个数字
-                long other = target - num;
+                int other = target - num;
                 //如果无效
                 if (other < 0) {
                     //本轮过
