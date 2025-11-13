9072c5302b5ed4292c6aa0981f6f0210e89a0783
Rock-Ayl <AnYongLiang4869@163.com>
2025-11-13 10:15:45 +0800

    修改：2025-11-13

---
+++ src/main/java/normal47/Code24.java
@@ -0,0 +1,101 @@
+package normal47;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author ayl
+ * @Date 2025-11-13
+ * 1823. 找出游戏的获胜者
+ * 算术评级: 3
+ * 第 236 场周赛
+ * Q2
+ * 同步题目状态
+ * <p>
+ * 1412
+ * 相关标签
+ * premium lock icon
+ * 相关企业
+ * 提示
+ * 共有 n 名小伙伴一起做游戏。小伙伴们围成一圈，按 顺时针顺序 从 1 到 n 编号。确切地说，从第 i 名小伙伴顺时针移动一位会到达第 (i+1) 名小伙伴的位置，其中 1 <= i < n ，从第 n 名小伙伴顺时针移动一位会回到第 1 名小伙伴的位置。
+ * <p>
+ * 游戏遵循如下规则：
+ * <p>
+ * 从第 1 名小伙伴所在位置 开始 。
+ * 沿着顺时针方向数 k 名小伙伴，计数时需要 包含 起始时的那位小伙伴。逐个绕圈进行计数，一些小伙伴可能会被数过不止一次。
+ * 你数到的最后一名小伙伴需要离开圈子，并视作输掉游戏。
+ * 如果圈子中仍然有不止一名小伙伴，从刚刚输掉的小伙伴的 顺时针下一位 小伙伴 开始，回到步骤 2 继续执行。
+ * 否则，圈子中最后一名小伙伴赢得游戏。
+ * 给你参与游戏的小伙伴总数 n ，和一个整数 k ，返回游戏的获胜者。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：n = 5, k = 2
+ * 输出：3
+ * 解释：游戏运行步骤如下：
+ * 1) 从小伙伴 1 开始。
+ * 2) 顺时针数 2 名小伙伴，也就是小伙伴 1 和 2 。
+ * 3) 小伙伴 2 离开圈子。下一次从小伙伴 3 开始。
+ * 4) 顺时针数 2 名小伙伴，也就是小伙伴 3 和 4 。
+ * 5) 小伙伴 4 离开圈子。下一次从小伙伴 5 开始。
+ * 6) 顺时针数 2 名小伙伴，也就是小伙伴 5 和 1 。
+ * 7) 小伙伴 1 离开圈子。下一次从小伙伴 3 开始。
+ * 8) 顺时针数 2 名小伙伴，也就是小伙伴 3 和 5 。
+ * 9) 小伙伴 5 离开圈子。只剩下小伙伴 3 。所以小伙伴 3 是游戏的获胜者。
+ * 示例 2：
+ * <p>
+ * 输入：n = 6, k = 5
+ * 输出：1
+ * 解释：小伙伴离开圈子的顺序：5、4、6、2、3 。小伙伴 1 是游戏的获胜者。
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= k <= n <= 500
+ * <p>
+ * <p>
+ * 进阶：你能否使用线性时间复杂度和常数空间复杂度解决此问题？
+ */
+public class Code24 {
+
+    public int findTheWinner(int n, int k) {
+        //初始化
+        List<Integer> list = new ArrayList<>(n);
+        //循环
+        for (int i = 1; i <= n; i++) {
+            //初始化节点
+            list.add(i);
+        }
+        //递归删除
+        remove(list, 0, k);
+        //返回
+        return list.get(0);
+    }
+
+    private void remove(List<Integer> list, int index, int k) {
+        //如果够了
+        if (list.size() < 2) {
+            //过
+            return;
+        }
+        //要删除的索引
+        int removeIndex = index + k - 1;
+        //如果越界
+        if (removeIndex >= list.size()) {
+            //删除之
+            removeIndex = removeIndex % list.size();
+        }
+        //删除
+        list.remove(removeIndex);
+        //递归删除
+        remove(list, removeIndex, k);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Code24().findTheWinner(5, 2));
+    }
+
+}
